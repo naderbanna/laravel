69584fde1ce59ef7c6209c24c70d4266bee3f884
naderbanna <naderbanna2@gmail.com>
2021-11-17 15:49:24 -0800

    controller updates, validation & error handling

--- app/Http/Controllers/UserApiController.php
+++ app/Http/Controllers/UserApiController.php
@@ -4,55 +4,59 @@ namespace App\Http\Controllers;
 
 use App\Models\myUser;
 use Illuminate\Http\Request;
+//use Illuminate\Validation\ValidationException;
 
 class UserApiController extends Controller
 {
     public function index()
-
     {
+        //display all users in db
         return myUser::all();
     }
 
     public function create(){
-        //ensure first name, last name, and email are entered when creating a user
+        //require all 3 fields to create new user
         request()->validate([
             'firstName' => 'required',
             'lastName' => 'required',
-            'email' => 'required',
+            'email' => 'required|unique:my_users,email',
         ]);
 
-        return myUser::create([
-            'firstName' => request('firstName'),
-            'lastName' => request('lastName'),
-            'email' => request('email'),
-        ]);
+        //return the newly created user
+        return [
+            myUser::create([
+                'firstName' => request('firstName'),
+                'lastName' => request('lastName'),
+                'email' => request('email'),
+            ])
+        ];
     }
-
-    public function update(myUser $user){
-        //require user id to update
-        $success = $user->update([
-            'firstName' => request('firstName'),
-            'lastName' => request('lastName'),
-            'email' => request('email'),
+    
+    //require user id to update
+    public function update(Request $request, myUser $user){
+       
+        //validate unique email
+        $request->validate([
+            'email' => 'unique:my_users,email'
         ]);
 
+        //update only the provided db fields
+        $success = $user->update($request->only(['firstName', 'lastName', 'email']));
+
+        //return true or false if update was successful
         return [
             'success' => $success    
         ];
-        //return true or false if update was successful
     }
 
+    //require user id to update
     public function delete(myUser $user){
-        //require user id to update
-        $success = $user->update([
-            'firstName' => request('firstName'),
-            'lastName' => request('lastName'),
-            'email' => request('email'),
-        ]);
+        //delete user
+        $success = $user->delete();
 
+        //return true or false if delete was successful
         return [
             'success' => $success    
         ];
-        //return true or false if update was successful
         }
 }
